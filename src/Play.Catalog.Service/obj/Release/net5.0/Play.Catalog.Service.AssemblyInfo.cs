168e21db99084a4bd1bb002552c192423a626dd1
Belzico <namichurri@gmail.com>
2022-07-30 17:18:44 +0200

    fix nugget

--- src/Play.Catalog.Service/obj/Release/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ src/Play.Catalog.Service/obj/Release/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -11,7 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
